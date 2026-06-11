e127366a765e2268147b842657c4c5fe3e0d57df
Choquet Nicolas <nicolachoquet06250@gmail.com>
2026-06-11 16:08:46 +0200

    fix: activer la normalisation websocket en production

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,6 +3,9 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import './style.css'
+import { installWebSocketUrlNormalizer } from './utils/websocket-url'
+
+installWebSocketUrlNormalizer()
 
 const app = createApp(App)
 const pinia = createPinia()
