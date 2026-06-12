6d4ef4654610d63727fd3fd321826fe0ffe2092c
Choquet Nicolas <nicolachoquet06250@gmail.com>
2026-06-12 15:36:00 +0000

    fix migrations

--- backend/migration/src/main.rs
+++ backend/migration/src/main.rs
@@ -1,8 +1,11 @@
 use sea_orm_migration::{prelude::*, sea_orm::Database};
 use std::{env, process};
+use dotenvy::dotenv;
 
 #[async_std::main]
 async fn main() {
+    dotenv().ok();
+
     if let Err(error) = run().await {
         eprintln!("{error}");
         process::exit(1);
@@ -10,7 +13,8 @@ async fn main() {
 }
 
 async fn run() -> Result<(), Box<dyn std::error::Error>> {
-    let database_url = env::var("DATABASE_URL")?;
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    println!("Using database URL: {database_url}");
     let command = env::args().nth(1).unwrap_or_else(|| "up".to_string());
     let db = Database::connect(&database_url).await?;
 
