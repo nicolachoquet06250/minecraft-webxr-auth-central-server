67d6169e6a1e2ab9a4efcffa445d4de2f7ec595f
Choquet Nicolas <nicolachoquet06250@gmail.com>
2026-06-12 15:59:32 +0200

    feat: add avatar entity model

---
+++ backend/src/models/avatar.rs
@@ -0,0 +1,35 @@
+use sea_orm::entity::prelude::*;
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Debug, PartialEq, DeriveEntityModel, Serialize, Deserialize)]
+#[sea_orm(table_name = "avatar")]
+pub struct Model {
+    #[sea_orm(primary_key, auto_increment = false)]
+    pub id: String,
+    pub user_id: String,
+    pub name: String,
+    pub base_kind: String,
+    pub is_active: bool,
+    #[sea_orm(column_type = "Text")]
+    pub texture_data: String,
+    pub created_at: DateTime,
+    pub updated_at: DateTime,
+}
+
+#[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
+pub enum Relation {
+    #[sea_orm(
+        belongs_to = "super::user::Entity",
+        from = "Column::UserId",
+        to = "super::user::Column::Id"
+    )]
+    User,
+}
+
+impl Related<super::user::Entity> for Entity {
+    fn to() -> RelationDef {
+        Relation::User.def()
+    }
+}
+
+impl ActiveModelBehavior for ActiveModel {}
