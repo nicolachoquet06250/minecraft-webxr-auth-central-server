a8168ea4af5ce1c573f20f053ffe83f6c84c6dd8
Choquet Nicolas <nicolachoquet06250@gmail.com>
2026-06-17 07:14:30 +0200

    feat: add friend request model

---
+++ backend/src/models/friend_request.rs
@@ -0,0 +1,19 @@
+use sea_orm::entity::prelude::*;
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Debug, PartialEq, DeriveEntityModel, Serialize, Deserialize)]
+#[sea_orm(table_name = "friend_request")]
+pub struct Model {
+    #[sea_orm(primary_key, auto_increment = false)]
+    pub id: String,
+    pub requester_user_id: String,
+    pub receiver_user_id: String,
+    pub status: String,
+    pub created_at: DateTime,
+    pub updated_at: DateTime,
+}
+
+#[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
+pub enum Relation {}
+
+impl ActiveModelBehavior for ActiveModel {}
