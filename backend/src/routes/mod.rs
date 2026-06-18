4d755c41356d73126ebee03fb481af14ae9e6ffb
Choquet Nicolas <nicolachoquet06250@gmail.com>
2026-06-18 08:08:53 +0200

    feat: register join ticket route module

--- backend/src/routes/mod.rs
+++ backend/src/routes/mod.rs
@@ -9,6 +9,7 @@ pub mod account_security;
 pub mod friends;
 pub mod friends_presence_ws;
 pub mod friends_realtime_ws;
+pub mod join_ticket;
 pub mod login_origin;
 pub mod openapi;
 pub mod openapi_paths;
